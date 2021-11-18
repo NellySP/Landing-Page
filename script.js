862b43af157985adad03565a1aa0d5105cae910d
NellySP <nelly.petren@gmail.com>
2021-11-18 15:21:47 +0100

    menu

--- script.js
+++ script.js
@@ -5,3 +5,21 @@
       this.classList.toggle('opened');
     });
   })();
+
+//   (function () {
+//     const toggle = document.querySelector('.myLinks');
+    
+//     toggle.addEventListener('click', function(e) {
+//       this.classList.toggle('opened');
+//     });
+//   })();
+//   hidden menu
+
+function hidden() {
+    const x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
