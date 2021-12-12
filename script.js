f81cf0f0d8c4521b8328c472ddb2c91acf018e04
patrikstaaf <staaf.patrik@gmail.com>
2021-12-12 23:02:09 +0100

    Fixed nav, hero and some other stuff...

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 // burger
 
-function MyFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+// function MyFunction() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
+// let menuButton = document.getElementById("nav-toggle");
+// menuButton.addEventListener("click", function (e) {
+//   MyFunction();
+// });
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 }
 
-let menuButton = document.getElementById("nav-toggle");
-menuButton.addEventListener("click", function (e) {
-  MyFunction();
-});
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+hamburger.addEventListener("click", mobileMenu);
